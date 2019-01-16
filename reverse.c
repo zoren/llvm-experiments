2b63705a7a2ecacc099c02475880ee30ccb6218d
Søren Sjørup <soren.sjorup@deondigital.com>
2019-01-16 01:12:12 +0100

    minor stuff

--- reverse.c
+++ reverse.c
@@ -8,15 +8,14 @@ int main() {
   long int index = 0;
 
   while((c = getchar())>=0) {
-    if(index >= 256) {
+    if(index > 255) {
       return -1;
     }
     if(c == nl) {
-      for(long int i = index-1;i >= 0;i--) {
+      for(long int i = index-1;i > -1;i--) {
         putchar(buffer[i]);
       }
       putchar(nl);
-      index = 0;
     } else {
       buffer[index] = c;
       index++;
